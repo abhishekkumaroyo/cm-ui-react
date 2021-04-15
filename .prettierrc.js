e24d7782eab8676bf00c55beedd42ae6bcdc06b4
abhishek <abhishek.kumar30@oyorooms.com>
2021-04-15 10:22:12 +0530

    prettier integrated

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    printWidth: 150,
+    tabWidth: 4,
+    semi: true,
+    bracketSpacing: true,
+    jsxBracketSameLine: true,
+    singleQuote: true,
+    trailingComma: 'none'
+};
